8c88b1b5d16a76d3762247f6d809f86184c7bdf2
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2024-03-28 13:05:12 +0530

    Update preInstall.sh

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,5 +1,13 @@
 #set env vars
 #set -o allexport; source .env; set +o allexport;
 
-mkdir -p ./data
-chown -R 1000:1000 ./data
+mkdir -p ./consume/;
+mkdir -p ./data/;
+mkdir -p ./export/;
+mkdir -p ./media/;
+
+
+chown 1000:1000 ./consume/
+chown 1000:1000 ./data/
+chown 1000:1000 ./export/
+chown 1000:1000 ./media/
